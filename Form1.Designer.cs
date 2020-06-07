ff5267e30dba4946350a6cc8c6fb4f6c8f593861
MrOxa <kingjos3333@gmail.com>
2020-06-07 22:19:05 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Movies_and_Series
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1SideMenu = new System.Windows.Forms.Panel();
+            this.panel1Movie = new System.Windows.Forms.Panel();
+            this.buttonStaredMovies = new System.Windows.Forms.Button();
+            this.buttonMoviesUpload = new System.Windows.Forms.Button();
+            this.buttonMoviesList = new System.Windows.Forms.Button();
+            this.button1Movie = new System.Windows.Forms.Button();
+            this.panel1Logo = new System.Windows.Forms.Panel();
+            this.SeriesButton = new System.Windows.Forms.Button();
+            this.panelSeries = new System.Windows.Forms.Panel();
+            this.buttonStaredSeries = new System.Windows.Forms.Button();
+            this.buttonSeriesUpload = new System.Windows.Forms.Button();
+            this.buttonSeriesList = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.panel1SideMenu.SuspendLayout();
+            this.panel1Movie.SuspendLayout();
+            this.panelSeries.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1SideMenu
+            // 
+            this.panel1SideMenu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1SideMenu.Controls.Add(this.buttonAbout);
+            this.panel1SideMenu.Controls.Add(this.buttonHelp);
+            this.panel1SideMenu.Controls.Add(this.panelSeries);
+            this.panel1SideMenu.Controls.Add(this.SeriesButton);
+            this.panel1SideMenu.Controls.Add(this.panel1Movie);
+            this.panel1SideMenu.Controls.Add(this.button1Movie);
+            this.panel1SideMenu.Controls.Add(this.panel1Logo);
+            this.panel1SideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1SideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panel1SideMenu.Name = "panel1SideMenu";
+            this.panel1SideMenu.Size = new System.Drawing.Size(233, 687);
+            this.panel1SideMenu.TabIndex = 0;
+            // 
+            // panel1Movie
+            // 
+            this.panel1Movie.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1Movie.Controls.Add(this.buttonStaredMovies);
+            this.panel1Movie.Controls.Add(this.buttonMoviesUpload);
+            this.panel1Movie.Controls.Add(this.buttonMoviesList);
+            this.panel1Movie.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1Movie.Location = new System.Drawing.Point(0, 156);
+            this.panel1Movie.Name = "panel1Movie";
+            this.panel1Movie.Size = new System.Drawing.Size(233, 143);
+            this.panel1Movie.TabIndex = 1;
+            // 
+            // buttonStaredMovies
+            // 
+            this.buttonStaredMovies.AutoSize = true;
+            this.buttonStaredMovies.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonStaredMovies.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonStaredMovies.FlatAppearance.BorderSize = 0;
+            this.buttonStaredMovies.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStaredMovies.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStaredMovies.Location = new System.Drawing.Point(0, 96);
+            this.buttonStaredMovies.Name = "buttonStaredMovies";
+            this.buttonStaredMovies.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonStaredMovies.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonStaredMovies.Size = new System.Drawing.Size(233, 52);
+            this.buttonStaredMovies.TabIndex = 2;
+            this.buttonStaredMovies.Text = "Started Movies";
+            this.buttonStaredMovies.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonStaredMovies.UseVisualStyleBackColor = false;
+            // 
+            // buttonMoviesUpload
+            // 
+            this.buttonMoviesUpload.AutoSize = true;
+            this.buttonMoviesUpload.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonMoviesUpload.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonMoviesUpload.FlatAppearance.BorderSize = 0;
+            this.buttonMoviesUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMoviesUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonMoviesUpload.Location = new System.Drawing.Point(0, 48);
+            this.buttonMoviesUpload.Name = "buttonMoviesUpload";
+            this.buttonMoviesUpload.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonMoviesUpload.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonMoviesUpload.Size = new System.Drawing.Size(233, 48);
+            this.buttonMoviesUpload.TabIndex = 1;
+            this.buttonMoviesUpload.Text = "Movie Upload";
+            this.buttonMoviesUpload.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonMoviesUpload.UseVisualStyleBackColor = false;
+            // 
+            // buttonMoviesList
+            // 
+            this.buttonMoviesList.AutoSize = true;
+            this.buttonMoviesList.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonMoviesList.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonMoviesList.FlatAppearance.BorderSize = 0;
+            this.buttonMoviesList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMoviesList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonMoviesList.Location = new System.Drawing.Point(0, 0);
+            this.buttonMoviesList.Name = "buttonMoviesList";
+            this.buttonMoviesList.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonMoviesList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonMoviesList.Size = new System.Drawing.Size(233, 48);
+            this.buttonMoviesList.TabIndex = 0;
+            this.buttonMoviesList.Text = "Movies List";
+            this.buttonMoviesList.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonMoviesList.UseVisualStyleBackColor = false;
+            // 
+            // button1Movie
+            // 
+            this.button1Movie.AutoSize = true;
+            this.button1Movie.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1Movie.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1Movie.FlatAppearance.BorderSize = 0;
+            this.button1Movie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1Movie.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1Movie.Location = new System.Drawing.Point(0, 108);
+            this.button1Movie.Name = "button1Movie";
+            this.button1Movie.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1Movie.Size = new System.Drawing.Size(233, 48);
+            this.button1Movie.TabIndex = 0;
+            this.button1Movie.Tag = "";
+            this.button1Movie.Text = "Movies";
+            this.button1Movie.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1Movie.UseVisualStyleBackColor = false;
+            // 
+            // panel1Logo
+            // 
+            this.panel1Logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1Logo.Location = new System.Drawing.Point(0, 0);
+            this.panel1Logo.Name = "panel1Logo";
+            this.panel1Logo.Size = new System.Drawing.Size(233, 108);
+            this.panel1Logo.TabIndex = 0;
+            // 
+            // SeriesButton
+            // 
+            this.SeriesButton.AutoSize = true;
+            this.SeriesButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.SeriesButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SeriesButton.FlatAppearance.BorderSize = 0;
+            this.SeriesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SeriesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.SeriesButton.Location = new System.Drawing.Point(0, 299);
+            this.SeriesButton.Name = "SeriesButton";
+            this.SeriesButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SeriesButton.Size = new System.Drawing.Size(233, 48);
+            this.SeriesButton.TabIndex = 2;
+            this.SeriesButton.Tag = "";
+            this.SeriesButton.Text = "Series";
+            this.SeriesButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SeriesButton.UseVisualStyleBackColor = false;
+            // 
+            // panelSeries
+            // 
+            this.panelSeries.Controls.Add(this.buttonStaredSeries);
+            this.panelSeries.Controls.Add(this.buttonSeriesUpload);
+            this.panelSeries.Controls.Add(this.buttonSeriesList);
+            this.panelSeries.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSeries.Location = new System.Drawing.Point(0, 347);
+            this.panelSeries.Name = "panelSeries";
+            this.panelSeries.Size = new System.Drawing.Size(233, 143);
+            this.panelSeries.TabIndex = 3;
+            // 
+            // buttonStaredSeries
+            // 
+            this.buttonStaredSeries.AutoSize = true;
+            this.buttonStaredSeries.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonStaredSeries.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonStaredSeries.FlatAppearance.BorderSize = 0;
+            this.buttonStaredSeries.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStaredSeries.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStaredSeries.Location = new System.Drawing.Point(0, 96);
+            this.buttonStaredSeries.Name = "buttonStaredSeries";
+            this.buttonStaredSeries.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonStaredSeries.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonStaredSeries.Size = new System.Drawing.Size(233, 48);
+            this.buttonStaredSeries.TabIndex = 5;
+            this.buttonStaredSeries.Text = "Started Series";
+            this.buttonStaredSeries.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonStaredSeries.UseVisualStyleBackColor = false;
+            // 
+            // buttonSeriesUpload
+            // 
+            this.buttonSeriesUpload.AutoSize = true;
+            this.buttonSeriesUpload.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonSeriesUpload.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonSeriesUpload.FlatAppearance.BorderSize = 0;
+            this.buttonSeriesUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSeriesUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSeriesUpload.Location = new System.Drawing.Point(0, 48);
+            this.buttonSeriesUpload.Name = "buttonSeriesUpload";
+            this.buttonSeriesUpload.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonSeriesUpload.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonSeriesUpload.Size = new System.Drawing.Size(233, 48);
+            this.buttonSeriesUpload.TabIndex = 4;
+            this.buttonSeriesUpload.Text = "Series Upload";
+            this.buttonSeriesUpload.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSeriesUpload.UseVisualStyleBackColor = false;
+            // 
+            // buttonSeriesList
+            // 
+            this.buttonSeriesList.AutoSize = true;
+            this.buttonSeriesList.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonSeriesList.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonSeriesList.FlatAppearance.BorderSize = 0;
+            this.buttonSeriesList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSeriesList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSeriesList.Location = new System.Drawing.Point(0, 0);
+            this.buttonSeriesList.Name = "buttonSeriesList";
+            this.buttonSeriesList.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonSeriesList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonSeriesList.Size = new System.Drawing.Size(233, 48);
+            this.buttonSeriesList.TabIndex = 3;
+            this.buttonSeriesList.Text = "Series List";
+            this.buttonSeriesList.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSeriesList.UseVisualStyleBackColor = false;
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.AutoSize = true;
+            this.buttonHelp.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonHelp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonHelp.FlatAppearance.BorderSize = 0;
+            this.buttonHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonHelp.Location = new System.Drawing.Point(0, 490);
+            this.buttonHelp.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonHelp.Size = new System.Drawing.Size(233, 48);
+            this.buttonHelp.TabIndex = 4;
+            this.buttonHelp.Tag = "";
+            this.buttonHelp.Text = "Help";
+            this.buttonHelp.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonHelp.UseVisualStyleBackColor = false;
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.AutoSize = true;
+            this.buttonAbout.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonAbout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonAbout.FlatAppearance.BorderSize = 0;
+            this.buttonAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonAbout.Location = new System.Drawing.Point(0, 538);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonAbout.Size = new System.Drawing.Size(233, 48);
+            this.buttonAbout.TabIndex = 5;
+            this.buttonAbout.Tag = "";
+            this.buttonAbout.Text = "About";
+            this.buttonAbout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonAbout.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(815, 687);
+            this.Controls.Add(this.panel1SideMenu);
+            this.Font = new System.Drawing.Font("Microsoft PhagsPa", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.panel1SideMenu.ResumeLayout(false);
+            this.panel1SideMenu.PerformLayout();
+            this.panel1Movie.ResumeLayout(false);
+            this.panel1Movie.PerformLayout();
+            this.panelSeries.ResumeLayout(false);
+            this.panelSeries.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1SideMenu;
+        private System.Windows.Forms.Panel panel1Movie;
+        private System.Windows.Forms.Button buttonStaredMovies;
+        private System.Windows.Forms.Button buttonMoviesUpload;
+        private System.Windows.Forms.Button buttonMoviesList;
+        private System.Windows.Forms.Button button1Movie;
+        private System.Windows.Forms.Panel panel1Logo;
+        private System.Windows.Forms.Button SeriesButton;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Panel panelSeries;
+        private System.Windows.Forms.Button buttonStaredSeries;
+        private System.Windows.Forms.Button buttonSeriesUpload;
+        private System.Windows.Forms.Button buttonSeriesList;
+    }
+}
+
